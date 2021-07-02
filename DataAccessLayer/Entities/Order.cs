39f9b27649d8390d9b5f859f22e3690aecc428ea
Artiom <48484710+PWMaestro@users.noreply.github.com>
2021-07-02 09:51:27 +0300

    Add order repo

---
+++ DataAccessLayer/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public ICollection<OrderElement> OrderElements { get; set; }
+    }
+}
