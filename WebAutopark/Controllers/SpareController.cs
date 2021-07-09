798e81cafb93b7993d0de718c0513c21796ac5fd
Artiom <48484710+PWMaestro@users.noreply.github.com>
2021-07-09 20:36:26 +0300

    Create Spares web page

---
+++ WebAutopark/Controllers/SpareController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessLayer.Repositories;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Entities;
+
+namespace WebAutopark.Controllers
+{
+    public class SpareController : Controller
+    {
+        private readonly IRepository<Spare> _spareRepo;
+
+        public SpareController(IRepository<Spare> spareRepo)
+        {
+            _spareRepo = spareRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_spareRepo.GetAll().OrderBy(sp => sp.SpareId));
+        }
+    }
+}
