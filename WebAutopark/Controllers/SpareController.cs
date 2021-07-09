d4d8ebe48f961a0e5307609d10430dd430eb403a
Artiom <48484710+PWMaestro@users.noreply.github.com>
2021-07-09 22:29:20 +0300

    Add full CRUD functionality to Spares

--- WebAutopark/Controllers/SpareController.cs
+++ WebAutopark/Controllers/SpareController.cs
@@ -22,5 +22,39 @@ namespace WebAutopark.Controllers
         {
             return View(_spareRepo.GetAll().OrderBy(sp => sp.SpareId));
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name)
+        {
+            _spareRepo.Create(new Spare { Name = name });
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            ViewBag.spare = _spareRepo.GetById(id);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(string name, int id)
+        {
+            _spareRepo.Update(new Spare { Name = name, SpareId = id });
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            _spareRepo.Delete(id);
+            return RedirectToAction("Index");
+        }
     }
 }
