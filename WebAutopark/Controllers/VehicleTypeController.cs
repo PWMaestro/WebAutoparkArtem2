d9a790eae490928dc308da560976e4b190ff8222
Artiom Sitko <48484710+PWMaestro@users.noreply.github.com>
2021-07-23 14:23:26 +0300

    Add CRUD for VehicleTypes

--- WebAutopark/Controllers/VehicleTypeController.cs
+++ WebAutopark/Controllers/VehicleTypeController.cs
@@ -23,6 +23,38 @@ namespace WebAutopark.Controllers
             var vehicleTypes = _vehicleTypeRepo.GetAll().OrderBy(e => e.VehicleTypeId);
             return View(vehicleTypes);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VehicleType vehicleType)
+        {
+            _vehicleTypeRepo.Create(vehicleType);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var vehicleType = _vehicleTypeRepo.GetById(id);
+            return View(vehicleType);
+        }
+
+        [HttpPost]
+        public IActionResult Update(VehicleType vehicleType)
+        {
+            _vehicleTypeRepo.Update(vehicleType);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _vehicleTypeRepo.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
